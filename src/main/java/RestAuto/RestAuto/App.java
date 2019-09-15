ef1197e8554b9734f2e7062623fd3fc085d5e407
vipulbhasin99 <vipulbhasin99@gmail.com>
2019-09-15 16:42:52 +0530

    first comit

---
+++ src/main/java/RestAuto/RestAuto/App.java
@@ -0,0 +1,22 @@
+package RestAuto.RestAuto;
+
+import com.google.common.reflect.Parameter;
+
+import io.restassured.RestAssured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    RestAssured.baseURI="";
+           
+    given().
+    		Param("","").
+    		
+}
