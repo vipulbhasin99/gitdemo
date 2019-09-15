92e225d2ec4f451e06eccd415243858a3a68b9c2
vipulbhasin99 <vipulbhasin99@gmail.com>
2019-09-15 17:51:30 +0530

    this is comit from GitDemo

--- src/main/java/RestAuto/RestAuto/App.java
+++ src/main/java/RestAuto/RestAuto/App.java
@@ -13,10 +13,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
     }
-    RestAssured.baseURI="";
-           
-    given().
-    		Param("","").
-    		
+    
 }
